108e7fc4de7641d10a04fb81f3de7712dc3cc7d0
sugaronsnow <anniekli01@gmail.com>
2020-01-12 03:53:07 -0600

    startActivity and createNewActivity finished created music fragment with add music button

--- app/src/main/java/com/example/musicpractice/ui/music/MusicFragment.java
+++ app/src/main/java/com/example/musicpractice/ui/music/MusicFragment.java
@@ -4,32 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.musicpractice.R;
 
-public class MusicFragment extends Fragment {
+public class MusicFragment extends Fragment implements View.OnClickListener {
 
-    private MusicViewModel galleryViewModel;
+    private MusicViewModel musicViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
+        musicViewModel =
                 ViewModelProviders.of(this).get(MusicViewModel.class);
         View root = inflater.inflate(R.layout.fragment_music, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        Button addMusicButton = root.findViewById(R.id.addMusicButton);
+        EditText newMusic = root.findViewById(R.id.newMusic);
+        Button confirmAddButton = root.findViewById(R.id.confirmAddButton);
+
+        newMusic.setVisibility(View.GONE);
+        confirmAddButton.setVisibility(View.GONE);
+
+        addMusicButton.setOnClickListener(unused -> onClick(root));
+
         return root;
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        view.findViewById(R.id.addMusicButton).setVisibility(View.GONE);
+        view.findViewById(R.id.newMusic).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.confirmAddButton).setVisibility(View.VISIBLE);
     }
 }
