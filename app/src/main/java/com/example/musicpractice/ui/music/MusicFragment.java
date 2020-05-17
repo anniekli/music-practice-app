1b04a97bd60af7192bab865d04fb1c57db9fdc10
sugaronsnow <anniekli01+github@gmail.com>
2020-05-17 00:46:50 -0500

    open MusicInfoFragment with button click

--- app/src/main/java/com/example/musicpractice/ui/music/MusicFragment.java
+++ app/src/main/java/com/example/musicpractice/ui/music/MusicFragment.java
@@ -2,22 +2,22 @@ package com.example.musicpractice.ui.music;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.musicpractice.MusicInfoFragment;
 import com.example.musicpractice.R;
 
 import java.util.ArrayList;
@@ -30,7 +30,7 @@ public class MusicFragment extends Fragment implements View.OnClickListener {
     Button addButton;
     private ListView musicList;
     private SearchView search;
-    private ArrayList<String> arrayList;
+    private ArrayList<String> musicArrayList;
     private ArrayAdapter<String> listViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -40,26 +40,19 @@ public class MusicFragment extends Fragment implements View.OnClickListener {
         musicViewModel =
                 ViewModelProviders.of(this).get(MusicViewModel.class);
 
-
         View root = inflater.inflate(R.layout.fragment_music, container, false);
-
-
-
         addButton = root.findViewById(R.id.addButton);
-        musicList = root.findViewById(R.id.listMode);
-
+        musicList = root.findViewById(R.id.musicList);
         search = root.findViewById(R.id.search_bar);
 
-
         addButton.setOnClickListener(this);
-        arrayList = new ArrayList<>();
-
+        musicArrayList = new ArrayList<>();
         listViewAdapter = new ArrayAdapter<>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
-                arrayList);
-        musicList.setAdapter(listViewAdapter);
+                musicArrayList);
 
+        musicList.setAdapter(listViewAdapter);
         return root;
     }
 
@@ -69,12 +62,25 @@ public class MusicFragment extends Fragment implements View.OnClickListener {
         if (!(search.getQuery().toString().trim().isEmpty())) {
             Log.i(TAG, "New button created: " + search.getQuery().toString());
 
-            arrayList.add(search.getQuery().toString());
-            musicList.setAdapter(listViewAdapter);
+            // adds button to screen and list
+            musicArrayList.add(search.getQuery().toString().trim());
+
+            // creates new fragment
+            Fragment newFragment = new MusicInfoFragment();
+            Bundle args = new Bundle();
+            args.putString("title", search.getQuery().toString());
+            newFragment.setArguments(args);
+
             musicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                     Log.i(TAG, "Item clicked: " + musicList.getItemAtPosition(position).toString());
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.replace(R.id.yay, newFragment, "newFragment");
+                    ft.addToBackStack(null);
+                    ft.commit();
+
                 }
             });
             listViewAdapter.sort(new Comparator<String>() {
